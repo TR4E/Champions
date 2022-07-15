12ad5475b380cc3812cc9a78780d81171d6ccd6c
TR4E <trae.rohleder@outlook.com>
2022-07-15 23:03:30 +1000

    Init

---
+++ src/main/java/me/trae/champions/Champions.java
@@ -0,0 +1,10 @@
+package me.trae.champions;
+
+import me.trae.core.framework.MiniPlugin;
+
+public class Champions extends MiniPlugin {
+
+    @Override
+    public void registerManagers() {
+    }
+}
