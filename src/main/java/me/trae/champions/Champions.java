10ac9521ea6efdea6bb3c1598f8186366fe28fc3
TR4E <trae.rohleder@outlook.com>
2022-07-18 01:28:47 +1000

    Fixes

--- src/main/java/me/trae/champions/Champions.java
+++ src/main/java/me/trae/champions/Champions.java
@@ -1,10 +1,12 @@
 package me.trae.champions;
 
+import me.trae.core.energy.EnergyManager;
 import me.trae.core.framework.MiniPlugin;
 
 public class Champions extends MiniPlugin {
 
     @Override
     public void registerManagers() {
+        addManager(EnergyManager.class);
     }
 }
