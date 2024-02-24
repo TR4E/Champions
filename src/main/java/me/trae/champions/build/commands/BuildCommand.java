bdce36e5e59dad8433a400c56af3f2ba022d3e09
TR4E <trae.rohleder@outlook.com>
2024-02-24 18:43:39 +1100

    Changes

--- src/main/java/me/trae/champions/build/commands/BuildCommand.java
+++ src/main/java/me/trae/champions/build/commands/BuildCommand.java
@@ -8,6 +8,7 @@ import me.trae.champions.role.menus.RoleSelectionMenu;
 import me.trae.core.client.Client;
 import me.trae.core.command.types.PlayerCommand;
 import me.trae.framework.shared.client.enums.Rank;
+import me.trae.framework.shared.gamer.global.types.GlobalGamer;
 import org.bukkit.entity.Player;
 
 public class BuildCommand extends PlayerCommand<BuildManager> {
@@ -17,19 +18,38 @@ public class BuildCommand extends PlayerCommand<BuildManager> {
     }
 
     @Override
-    public void execute(final Player player, final Client client, final String[] args) {
+    public String getDescription() {
+        return "Customize your Role Build";
+    }
+
+    @Override
+    public void execute(final Player player, final Client client, final GlobalGamer globalGamer, final String[] args) {
         if (args.length == 0) {
             new RoleSelectionMenu(this.getInstance().getManagerByClass(RoleManager.class), player) {
                 @Override
                 public void onButtonClick(final Player player, final Role role) {
-                    new BuildCustomizationMenu(BuildCommand.this.getManager(), player, role) {
-                        @Override
-                        public Role getRole() {
-                            return role;
-                        }
-                    };
+                    BuildCommand.this.open(player, role);
                 }
             };
+            return;
         }
+
+        if (args.length == 1) {
+            final Role role = this.getInstance().getManagerByClass(RoleManager.class).searchRole(player, args[0], true);
+            if (role == null) {
+                return;
+            }
+
+            this.open(player, role);
+        }
+    }
+
+    private void open(final Player player, final Role role) {
+        new BuildCustomizationMenu(BuildCommand.this.getManager(), player, role) {
+            @Override
+            public Role getRole() {
+                return role;
+            }
+        };
     }
 }
