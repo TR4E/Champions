c0afb538f8d850dab2fa90b595b36146edf4985b
TR4E <trae.rohleder@outlook.com>
2024-02-24 18:50:43 +1100

    Changes

--- src/main/java/me/trae/champions/skill/skills/knight/BullsCharge.java
+++ src/main/java/me/trae/champions/skill/skills/knight/BullsCharge.java
@@ -20,7 +20,9 @@ public class BullsCharge extends ActiveSkill<Knight, SkillData> {
     public BullsCharge(final Knight module) {
         super(module, ActiveSkillType.AXE);
 
-        this.addPrimitive("Duration", 5000L);
+        this.addPrimitive("Recharge", 10_000L);
+
+        this.addPrimitive("Duration", 5_000L);
         this.addPrimitive("Amplifier", 2);
     }
 
@@ -42,7 +44,7 @@ public class BullsCharge extends ActiveSkill<Knight, SkillData> {
 
     @Override
     public void onActivate(final Player player, final int level) {
-        if (this.getInstance().getManagerByClass(RechargeManager.class).add(player, this.getName(), 5000L, true)) {
+        if (this.getInstance().getManagerByClass(RechargeManager.class).add(player, this.getName(), this.getPrimitiveCasted(Long.class, "Recharge"), true)) {
             UtilEntity.addPotionEffect(player, PotionEffectType.SPEED, this.getPrimitiveCasted(Long.class, "Duration"), this.getPrimitiveCasted(Integer.class, "Amplifier"));
 
             UtilMessage.simpleMessage(player, this.getModule().getName(), "You used <green><var></green>.", Collections.singletonList(this.getDisplayName(level)));
