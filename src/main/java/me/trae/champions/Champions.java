69b92af541861c35011bf05346315769762f4140
TR4E <trae.rohleder@outlook.com>
2024-02-25 03:34:21 +1100

    Fixes

--- src/main/java/me/trae/champions/Champions.java
+++ src/main/java/me/trae/champions/Champions.java
@@ -3,4 +3,9 @@ package me.trae.champions;
 import me.trae.core.framework.SpigotFactory;
 
 public abstract class Champions extends SpigotFactory {
+
+    @Override
+    public String getName() {
+        return "Champions";
+    }
 }
